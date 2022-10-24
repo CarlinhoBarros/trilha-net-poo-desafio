77dedce0c1d3ae5e9090d20b628de1ccdaf213b8
CarlinhoBarros <carlosmonteiro92@hotmail.com>
2022-10-23 23:01:29 -0300

    Alteração na visibilidade das propriedades (Modelo, IMEI e Memoria) de public para private.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,9 +4,9 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
-        public int Memoria { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
